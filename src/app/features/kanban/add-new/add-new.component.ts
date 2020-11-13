ae1712cf8d85ac39ed267e943ef60e215f3916fb
Tommy <lego2003@gmail.com>
2020-11-13 22:49:14 +0700

    ok

--- src/app/features/kanban/add-new/add-new.component.ts
+++ src/app/features/kanban/add-new/add-new.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { JobListService } from '../services/job-list.service';
+import { JobListService } from '../services/list.service';
 
 @Component({
   selector: 'app-add-new',
